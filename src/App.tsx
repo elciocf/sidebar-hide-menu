459404d624cd3f104c382b3b9d63cfc4e5c47081
Elcio Freitas <elcio.cf@gmail.com>
2021-03-13 17:52:38 -0300

    Add sidebar menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import { RepositoryList } from './components/RepositoryList'
+import { Sidebar } from './components/Sidebar'
+import {Home} from './components/Pages/Home'
+import {Products} from './components/Pages/Products'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './styles/global.scss' 
 
 export function App(){
 
     return (
-        <RepositoryList />
+        <>
+         <Router>
+          <Sidebar />
+          <Switch>
+              <Route path='/' exact component={Home} />
+              <Route path='/home' component={Home} />
+              <Route path='/products' component={Products} />
+          </Switch>
+        </Router>
+        </>
     )
 }
---
+++ src/components/Pages/Products.tsx
@@ -0,0 +1,8 @@
+export function Products(){
+    
+    return ( 
+        <div className="products">
+            <h1>PRODUTCS</h1>
+        </div>
+    )
+}
